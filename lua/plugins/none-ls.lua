6b71c3ee902d47fd04bb4354b2605accf6e383d8
Sohail Yunus <sohail.yunus@khumbusystems.com>
2024-02-02 13:44:57 +0530

    Added noice and isort.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -21,6 +21,7 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.ruff,
 				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
 				null_ls.builtins.diagnostics.ruff,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.eslint_d,
